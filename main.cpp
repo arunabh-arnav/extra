7c64e5b3737f08d5b092677ba1b92843a539d2de
Arunabh Arnav <arunabh.arnav@gmail.com>
2017-03-28 01:35:13 -0400

    RR working

--- main.cpp
+++ main.cpp
@@ -84,7 +84,7 @@ void initialize_readers(){
     string junk;
     rfile.open("rfile");
     getline(rfile,junk);    // skipping the first line which holds the size
-    ifile.open("input2");
+    ifile.open("input4");
 }
 
 // Send out a new random number each time it is called
@@ -128,27 +128,38 @@ void get_processes(){
     }
 }
 
+// Get start time of the next event from EVQUEUE
+int get_next_event_time(){
+    if(EVQUEUE.empty())
+        return inf_time;
+    return (EVQUEUE.top())->timeline;
+}
+
 // Get new processes in the current time range
-bool get_current_processes(){
+bool get_current_processes(bool advance){
     cprocesses.clear();
     if(next_pid>=processes.size())
         return false;
-    if(cprocesses.empty()){
-        time_elapsed = processes[next_pid]->AT;
-        cprocesses.push_back(processes[next_pid]);
-        if(descriptive){
-            cout<<time_elapsed<<" "<<processes[next_pid]->PID<<" "<<0
-            <<": "<<"CREATED"<<" -> "<<"READY"<<endl;
+    if(cprocesses.empty() && advance){                              // When RUNQUEUE is empty and it is absolutely nec. to advance timefron
+        if(processes[next_pid]->AT<=get_next_event_time()){             // **** VERIFY, TODO
+            time_elapsed = processes[next_pid]->AT;                     // **** VERIFY, uncommented: TODO
+            cprocesses.push_back(processes[next_pid]);
+            if(descriptive){
+                cout<<time_elapsed<<" "<<processes[next_pid]->PID<<" "<<0
+                <<": "<<"CREATED"<<" -> "<<"READY"<<endl;
+            }
+            next_pid++;
         }
-        next_pid++;
     }
     while(next_pid<processes.size() && processes[next_pid]->AT<=time_elapsed){
-        cprocesses.push_back(processes[next_pid]);
-        if(descriptive){
-            cout<<time_elapsed<<" "<<processes[next_pid]->PID<<" "<<0
-            <<": "<<"CREATED"<<" -> "<<"READY"<<endl;
+        if(processes[next_pid]->AT<=get_next_event_time()){             // **** VERIFY, TODO
+            cprocesses.push_back(processes[next_pid]);
+            if(descriptive){
+                cout<<time_elapsed<<" "<<processes[next_pid]->PID<<" "<<0
+                <<": "<<"CREATED"<<" -> "<<"READY"<<endl;
+            }
+            next_pid++;
         }
-        next_pid++;
     }
     return true;
 }
@@ -183,19 +194,15 @@ void delete_event(){
     }
 }
 
-// Get start time of the next event from EVQUEUE
-int get_next_event_time(){
-    if(EVQUEUE.empty())
-        return inf_time;
-    return (EVQUEUE.top())->timeline;
-}
+
 
 // ROUND-ROBIN Scheduler
 class RR:public Scheduler{
             queue<process*> RUNQUEUE;
             public:
             bool add_processes(){
-                if(get_current_processes()){
+                bool advance = RUNQUEUE.size()==0?true:false;   // advance the time_elapsed to next available proc if RUNQUEUE is empty
+                if(get_current_processes(advance)){
                     for(int i=0;i<cprocesses.size();i++){
                         RUNQUEUE.push(cprocesses[i]);
                     }
@@ -211,7 +218,7 @@ class RR:public Scheduler{
             }
 
             process* get_next_process(){
-                add_processes();                        // fetches all current processes
+//                add_processes();                        // fetches all current processes
                 int n = RUNQUEUE.size();
                 if(n>0){
                     process* proc = RUNQUEUE.front();       // selects the correct process to be run (to be sent to the EVQUEUE later)
@@ -221,11 +228,11 @@ class RR:public Scheduler{
                 return NULL;
             }
 
-            void reduce_dynamic_priority(process* proc){
-                if(proc->dprio==0)
-                    proc->dprio = proc->sprio-1;
-                else
-                    proc->dprio--;
+            void reduce_dynamic_priority(process* proc){    // **** TODO commented out for testing purposes
+//                if(proc->dprio==0)
+//                    proc->dprio = proc->sprio-1;
+//                else
+//                    proc->dprio--;
             }
 
             void reset_dynamic_priority(process* proc){
@@ -259,9 +266,10 @@ void simulate_processes(Scheduler* scheduler){
         oldstate = e->newstate;    // the next state will be the oldstate for the next event, if an event is created
         process* proc = e->process_;
         proc->last_state = oldstate;
+        proc->prev_state_time = time_elapsed - proc->state_time_start;
         switch(oldstate){
-            proc->prev_state_time = time_elapsed - proc->state_time_start;
             case READY:
+//                scheduler->add_processes();     // **** VERIFY: TODO
                 proc->state_time_start = time_elapsed;
                 if(descriptive){
                     cout<<time_elapsed<<" "<<proc->PID<<" "<<proc->prev_state_time
@@ -279,6 +287,7 @@ void simulate_processes(Scheduler* scheduler){
 
             case RUNNG:
                 {
+//                scheduler->add_processes();     // **** VERIFY: TODO
                 int cb;
                 proc->cpu_wait_time += proc->prev_state_time;
                 if(proc->cbremaining>0){
@@ -300,15 +309,17 @@ void simulate_processes(Scheduler* scheduler){
                     proc->state_time_start = time_elapsed;
                     time_elapsed += tc;
                     CPU_time += tc;
-                    if(descriptive){
-                        cout<<time_elapsed<<" "<<proc->PID<<" "<<proc->prev_state_time
-                        <<": "<<"Done"<<endl;
-                    }
+//                    if(descriptive){
+//                        cout<<time_elapsed<<" "<<proc->PID<<" "<<cb         // **** CONFIRM "cb" :TODO
+//                        <<": "<<"Done"<<endl;
+//                    }
                     proc->TC -= tc;
                     proc->finishtime = time_elapsed;
-                    proc = NULL;                        // **** So that the call_scheduler works: TODO
-                    call_scheduler=true;
-                    cpu_block = time_elapsed;           // to prohibit a READY process to be scheduled until CPU is active
+                    put_event(&proc,oldstate,BLOCK,time_elapsed);
+                    cpu_block = time_elapsed;
+//                    proc = NULL;                        // **** So that the call_scheduler works: TODO
+//                    call_scheduler=true;
+//                    cpu_block = time_elapsed;           // to prohibit a READY process to be scheduled until CPU is active
                 }
                 // 2. Check if CPU burst ends before or at quantum: BLOCKING STATE NEXT
                 else if(cb<=QUANTUM){
@@ -338,22 +349,33 @@ void simulate_processes(Scheduler* scheduler){
 
             case BLOCK:
                 {
-                proc->state_time_start = time_elapsed;
-                int io = get_new_burst(proc->IO);
-                if(descriptive){
-                    cout<<time_elapsed<<" "<<proc->PID<<" "<<proc->prev_state_time
-                    <<": "<<"RUNNG"<<" -> "<<"BLOCK "
-                    <<" "<<"ib="<<io<<" rem="<<proc->TC<<endl;
-                }
-                proc->blocktime += io;
-                IO_time += io;
+//                scheduler->add_processes();     // **** VERIFY: TODO
+                if(proc->TC==0){
+                    if(descriptive){
+                        cout<<time_elapsed<<" "<<proc->PID<<" "<<proc->prev_state_time         // **** CONFIRM "cb" :TODO
+                        <<": "<<"Done"<<endl;
+                    }
+                    proc = NULL;                        // **** So that the call_scheduler works: TODO
+                    call_scheduler=true;
+                }else{
+                    proc->state_time_start = time_elapsed;
+                    int io = get_new_burst(proc->IO);
+                    if(descriptive){
+                        cout<<time_elapsed<<" "<<proc->PID<<" "<<proc->prev_state_time
+                        <<": "<<"RUNNG"<<" -> "<<"BLOCK "
+                        <<" "<<"ib="<<io<<" rem="<<proc->TC<<endl;
+                    }
+                    proc->blocktime += io;
+                    IO_time += io;
 
-                put_event(&proc,oldstate,READY,time_elapsed+io);
-                call_scheduler = true;
+                    put_event(&proc,oldstate,READY,time_elapsed+io);
+                    call_scheduler = true;
+                }
                 }
                 break;
 
             case PREEMPT:
+//                scheduler->add_processes();     // **** VERIFY: TODO
                 proc->state_time_start = time_elapsed;
                 if(descriptive){
                     cout<<time_elapsed<<" "<<proc->PID<<" "<<proc->prev_state_time
@@ -362,24 +384,32 @@ void simulate_processes(Scheduler* scheduler){
                     <<" prio="<<proc->dprio<<endl;
                 }
                 scheduler->add_to_runqueue(proc);
-                proc = NULL;
-                call_scheduler = true;
+//                if(time_elapsed>cpu_block){                     // **** VERIFY: TODO check if this condition needs to be added
+                    proc = NULL;
+                    call_scheduler = true;
+//                }
                 break;
             default: exit(2);
         }
         delete_event();
         delete e;
+        scheduler->add_processes();
         if(call_scheduler){
             if(get_next_event_time()==time_elapsed){
                 continue;
             }
+//            if(next_pid<processes.size() && get_next_event_time()<=processes[next_pid]->AT){   // **** VERIFY TODO
+//                scheduler->add_processes();             // **** VERIFFY TODO
+//                continue;
+//            }
             call_scheduler=false;
             if(proc == NULL){
                 proc = scheduler->get_next_process();
                 if(proc == NULL)
                     continue;
             }
-        put_event(&proc,proc->last_state,RUNNG,time_elapsed);
+        int run_evt_time = max(time_elapsed,cpu_block);         // **** VERIFY: TODO
+        put_event(&proc,proc->last_state,RUNNG,run_evt_time);   // **** VERIFY: TODO
         }
     }
 }
@@ -390,71 +420,12 @@ int main(){
     get_processes();                                            // reads the file contents into a vector called processes
     Scheduler* scheduler = NULL;
     scheduler = new RR();
-    QUANTUM = 5;
+    QUANTUM = 2;
     // Initialization of processes and events prior to the scheduling
+    scheduler->add_processes();                                 // **** VERIFY: TODO
     CURRENT_PROCESS = scheduler->get_next_process();            // the scheduler adds the processes in its run-queues, returns the selected process, modifies(removes from)
                                                                 // run-queue with updated time_elapsed
     put_event(&CURRENT_PROCESS, CURRENT_PROCESS->last_state, RUNNG, time_elapsed);  // ANNULLS the CURRENT_PROCESS, and adds the process to the EVQUEUE
     simulate_processes(scheduler);                              // With at least one event in EVQUEUE, start the simulation of process execution
     return 0;
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// to be dealt with later
-void print_process_timeline(process* proc){
-    time_elapsed = max(proc->AT,time_elapsed);
-    cout<<time_elapsed<<" "<<proc->PID<<" "<<0
-        <<": "<<"CREATED"<<" -> "<<"READY"<<endl;
-    int last_burst=0;
-    while(proc->TC>0){  // while process is still not done
-        int cb =get_new_burst(proc->CB);
-        if(cb>=proc->TC){
-            cb = proc->TC;
-            proc->TC = 0;
-        }
-        else{
-            proc->TC -= cb;
-        }
-        last_burst = cb;
-        cout<<time_elapsed<<" "<<proc->PID<<" "<<0
-        <<": "<<"READY"<<" -> "<<"RUNNG"
-        <<" "<<"cb="<<cb<<" rem="<<proc->TC+cb
-        <<" prio="<<proc->dprio<<endl;
-        time_elapsed+=cb;
-        if(proc->TC==0)
-            break;
-        int io = get_new_burst(proc->IO);
-        cout<<time_elapsed<<" "<<proc->PID<<" "<<cb
-        <<": "<<"RUNNG"<<" -> "<<"BLOCK "
-        <<" "<<"ib="<<io<<" rem="<<proc->TC<<endl;
-        time_elapsed+=io;
-
-        cout<<time_elapsed<<" "<<proc->PID<<" "<<io
-        <<": "<<"BLOCK"<<" -> "<<"READY"<<endl;
-    }
-    cout<<time_elapsed<<" "<<proc->PID<<" "<<last_burst
-        <<": "<<"Done"<<endl;
 }
